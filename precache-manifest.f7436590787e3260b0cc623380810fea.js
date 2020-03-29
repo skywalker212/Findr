8af6064588b71fb1cf1a908a2de8b7291432213d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-29 13:04:51 +0000

    Deploy skywalker212/findr to github.com/skywalker212/findr.git:gh-pages

---
+++ precache-manifest.f7436590787e3260b0cc623380810fea.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b58cf7ab00c85a8a3401e416fa073a8",
+    "url": "/findr/index.html"
+  },
+  {
+    "revision": "a42fe08a9bc89da43f27",
+    "url": "/findr/static/css/2.62e8d5ba.chunk.css"
+  },
+  {
+    "revision": "7a50d13c6278beab148d",
+    "url": "/findr/static/css/main.40fdbe37.chunk.css"
+  },
+  {
+    "revision": "a42fe08a9bc89da43f27",
+    "url": "/findr/static/js/2.d58818fd.chunk.js"
+  },
+  {
+    "revision": "a51ff95b3f79621f2472576f8fd57509",
+    "url": "/findr/static/js/2.d58818fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a50d13c6278beab148d",
+    "url": "/findr/static/js/main.49b36811.chunk.js"
+  },
+  {
+    "revision": "b96321a162c837876a8c",
+    "url": "/findr/static/js/runtime-main.872985b9.js"
+  },
+  {
+    "revision": "fd0f11482075048cd3181f47bf8c05a2",
+    "url": "/findr/static/media/mario.fd0f1148.png"
+  },
+  {
+    "revision": "fd5728f2cf777b06b966d05c0c823dc9",
+    "url": "/findr/static/media/spritesheet.fd5728f2.svg"
+  }
+]);
